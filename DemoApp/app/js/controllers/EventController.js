2063b5e92622438afd710eccb8ccb425cb23092a
Chris Wilsher <chris_wilsher@hotmail.com>
2014-09-24 14:13:29 -0600

    initial checkin of demo app code

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,29 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location:{
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass'
+                },
+                {
+                    name: 'Scopes for fun and profit'
+                },
+                {
+                    name: 'Well Behaved Controllers'
+                }
+            ]
+        }
+    }
+);
